8b100dcde5b2912b730d909d04edae7d7edcd21a
dumiensky <dumiensky@icloud.com>
2024-06-16 22:18:40 +0200

    current jobs/stories displays

--- ProjectManager/Models/Statics/Theme.cs
+++ ProjectManager/Models/Statics/Theme.cs
@@ -4,5 +4,12 @@ namespace ProjectManager.Models.Statics;
 
 public class Theme
 {
-	public static readonly MudTheme Default = new();
+	public static readonly MudTheme Default = new()
+	{
+		Palette = new PaletteLight
+		{
+			Background = Colors.Grey.Lighten5
+		},
+		PaletteDark = new PaletteDark()
+	};
 }
