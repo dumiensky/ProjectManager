59890e593b77ecad356821d77e074a8e08bdc4f0
dumiensky <dumiensky@icloud.com>
2024-06-17 13:46:05 +0200

    Menu implementation, user selector bugfix

--- ProjectManager/Services/ProjectContextHooks.cs
+++ ProjectManager/Services/ProjectContextHooks.cs
@@ -4,15 +4,7 @@ namespace ProjectManager.Services;
 
 public class ProjectContextHooks : IProjectContextHooks
 {
-	public event EventHandler? OnProjectListRefresh; 
 	public event EventHandler<Guid>? OnCurrentProjectSet;
-
-	public async Task RefreshProjectList()
-	{
-		OnProjectListRefresh?.Invoke(this, EventArgs.Empty);
-
-		await Task.Delay(TimeSpan.FromMilliseconds(200));
-	}
 
 	public void SetCurrentProject(Guid projectId)
 	{
